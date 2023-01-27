c1b0e3d52b7335cc25c73f554e74414b1dcc554a
Grissela <108595899+Grissela@users.noreply.github.com>
2023-01-27 09:45:33 -0500

    Form Reactive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sesion28';
+  public formLogin !: FormGroup
+  constructor(private formBuilder: FormBuilder){}
+  ngOnInit(): void{
+    this.formLogin = this.formBuilder.group({
+      username:['',
+      [Validators.required,Validators.minLength(5)],
+      
+    ],
+      password:['',
+      [Validators.required,Validators.minLength(5)]
+    ],
+    })
+  }
+
+  enviar(){
+    
+  }
 }
