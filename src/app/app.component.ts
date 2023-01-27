8ad805cc0a686c5a5634339c35ae6c0ebddee401
Grissela <108595899+Grissela@users.noreply.github.com>
2023-01-27 15:26:46 -0500

    Modificando diseño

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import Swal from 'sweetalert2'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -27,9 +27,25 @@ export class AppComponent implements OnInit {
   enviar(){
     if(this.formLogin.valid){
       this.resultado = "Estoy logeado" 
+      Swal.fire({
+        title: '¡Estas logeado!',
+        text: 'Has accedido',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Check_green_icon.svg/2048px-Check_green_icon.svg.png',
+        imageWidth: 400,
+        imageHeight: 300,
+        imageAlt: 'Custom image',
+      })
     }
     else{
       this.resultado = "No estoy logeado"
+      Swal.fire({
+        title: '¡No estas logeado!',
+        text: 'Credenciales incorrectas',
+        imageUrl: 'https://www.freeiconspng.com/thumbs/error-icon/sign-error-icon-10.png',
+        imageWidth: 400,
+        imageHeight: 300,
+        imageAlt: 'Custom image',
+      })
     }
    
   }
